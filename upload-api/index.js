f420e6192ae1ae0a56f0f4e4be081b1e3d16de31
Mohammad Sohail <msohailcs@outlook.com>
2020-04-04 23:53:53 +0100

    organised files & added express backend

---
+++ upload-api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+const upload = multer({
+    dest: './uploads/'
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.json({ file: req.file});
+});
+
+app.listen(3344, () => console.log("running on localhost:3344"));
